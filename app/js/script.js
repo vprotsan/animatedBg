bc42c7e766ff911cc54dae18ca54ec72bb89c2da
Valeria Protsan <valeriyapro@gmail.com>
2020-05-22 21:18:43 -0400

    2img

--- app/js/script.js
+++ app/js/script.js
@@ -1,44 +1,64 @@
-const element = "animated_background"
+//
+// CANVAS
+//
+// let canvas = document.getElementById("canvas"),
+// ctx = canvas.getContext("2d");
+//
+// canvas.width = 903;
+// canvas.height = 657;
+//
+// let background = new Image();
+// background.src = "http://www.samskirrow.com/background.png";
+//
+// // Make sure the image is loaded first otherwise nothing will draw.
+// background.onload = function(){
+//     ctx.drawImage(background,0,0);
+// }
+//
+// END CANVAS
+//
+//
+// element.style.background = "url('a.png') 0 100%, url('b.png') 50px 50px ";
 
-//return current background img
+const element = "animated_bg_img"
+
+//FOR DIV
 function getCurrentBg(el){
-  return document.getElementById(el).style.backgroundImage
+  return document.getElementById(el).src
 }
 
 //apply new bg image
 function applyBackground(fullUrl, el){
-  return document.getElementById(el).style.backgroundImage = "url(" + fullUrl + ")";
+  return document.getElementById(el).src = fullUrl
 }
 
 //generate full path for bg img
 function generateBgFullPath(image){
-  return "../images/bg/" + image
+  return "./images/bg/" + image
 }
 
 //generate full path for animation img
 function generateAnimationFullPath(image){
-  return "../images/animation/" + image + '.gif'
+  return "./images/animation/" + image + '.gif'
 }
 
 function isTimeToChangeBackground(){
-
   let imgName = ""
   let imgExtention = ".png"
 
-  // let rightNow = new Date()
-  // let hours = rightNow.getHours()
-  // let minutes = rightNow.getMinutes()
+  let rightNow = new Date()
+  let hours = rightNow.getHours()
+  let minutes = rightNow.getMinutes()
 
-  //for testing
-  // console.log(hours, minutes)
-  let hours = 10
-  let minutes = 23
-  console.log(hours, minutes)
+  //for TESTING
+  hours = 4
+  minutes = 23
   //end for testing
+
+  console.log(`Hours: ${hours} Minutes: ${minutes}`)
 
   if (0 <= hours && hours < 2){
     imgName = "v10-0000"
-    console.log("this condition")
   } else if (2 <= hours && (hours >= 3 && minutes <= 14)){
     imgName = "v10-0200"
   } else if ((3 >= hours && minutes > 14) && hours < 4){
@@ -64,21 +84,16 @@ function isTimeToChangeBackground(){
   } else if (22 <= hours && hours < 0){
     imgName = "v10-2200"
   } else {
-    console.log("default")
-    // imgName = "v10-0000"
+    imgName = "v10-0000"
   }
 
   imgName += imgExtention
-  console.log(imgName)
 
   let currBgFullPath = getCurrentBg(element)
   let currBgArray = currBgFullPath.split("/")
-  currBg = currBgArray[2].replace(/[{()}"]/g, '');
+  currBg = currBgArray.slice(-1)[0].replace(/[{()}"]/g, '');
 
-  //test
-  console.log(currBg)
-  console.log(imgName)
-  //end test
+  console.log(`Current background: ${currBg}, calculated bg: ${imgName}`)
 
   if (currBg === imgName) return
 
@@ -87,3 +102,5 @@ function isTimeToChangeBackground(){
 }
 
 isTimeToChangeBackground()
+
+setInterval(() => isTimeToChangeBackground(), 3000); //check for update every minute
